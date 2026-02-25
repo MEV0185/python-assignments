4222dc6f32c5144986fe878ef452e972134bbabf
MEV0185 <emarc@ivytech.edu>
2026-02-24 21:08:54 -0500

    Add files via upload
    Add rain sentence Python assignment

---
+++ rain_sentence.py
@@ -0,0 +1,13 @@
+"""
+Program: Rain Sentence
+Author: Emmanuel
+Description:
+    This program prints a sentence using variables.
+"""
+
+c = "cats"
+d = "dogs"
+
+s = "It is raining " + c + " and " + d + "."
+
+print(s)
